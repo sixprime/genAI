150d8dd0a708d3f434f3365bea2b88843a1dbcf3
sixprime <andrei.pastramagiu@gmail.com>
2019-10-10 02:06:23 -0400

    Added Bootstrap design

--- genai/static/js/style_transfer.js
+++ genai/static/js/style_transfer.js
@@ -13,28 +13,6 @@ function readURL(input) {
     }
 }
 
-/*function base64ToBlob(base64, mime) 
-{
-    mime = mime || '';
-    var sliceSize = 1024;
-    var byteChars = atob(base64);
-    var byteArrays = [];
-
-    for (var offset = 0, len = byteChars.length; offset < len; offset += sliceSize) {
-        var slice = byteChars.slice(offset, offset + sliceSize);
-
-        var byteNumbers = new Array(slice.length);
-        for (var i = 0; i < slice.length; i++) {
-            byteNumbers[i] = slice.charCodeAt(i);
-        }
-
-        var byteArray = new Uint8Array(byteNumbers);
-        byteArrays.push(byteArray);
-    }
-
-    return new Blob(byteArrays, {type: mime});
-}*/
-
 var startStyleTransferTask = function () {
     var image = $('#user_picture').attr('src');
     if (image == 'static/img/image_preview.jpg') {
